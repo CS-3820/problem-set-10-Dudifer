ae52270b3841ee17d99c55aa07d94fb0f55fef3d
janyberg <janyberg@uiowa.edu>
2024-11-15 17:08:20 -0600

    Update Problems10.hs

--- Problems10.hs
+++ Problems10.hs
@@ -218,18 +218,17 @@ smallStep (Recall, acc) = Just (acc, acc) -- Return current accumulator
 smallStep (Throw m, acc)
   | isValue m = Just (Throw m, acc)
   | otherwise = fmap (\(m', acc') -> (Throw m', acc')) (smallStep (m, acc))
-smallStep (Catch m y h, acc)
-  | isValue m = Just (m, acc) -- `m` successfully evaluates to a value
-  | case m of
-      Throw w -> Just (subst y w h, acc) -- Handle exception by substitution
-      _       -> (Catch m' y h, acc') (smallStep (m, acc))
---      _       -> fmap (\(m', acc') -> (Catch m' y h, acc')) (smallStep (m, acc)) 
-smallStep (App (Lam x body) arg), acc
+smallStep (App (Lam x body) arg, acc)
   | isValue arg = Just (subst x arg body, acc) -- Function application
   | otherwise = fmap (\(arg', acc') -> ((App (Lam x body) arg)', acc')) (smallStep (arg, acc))
 smallStep (App m1 m2), acc
   | isValue m1 = fmap (\(m2', acc') -> (App m1 m2', acc')) (smallStep (m2, acc))
   | otherwise = fmap (\(m1', acc') -> (App m1' m2, acc')) (smallStep (m1, acc))
+smallStep (Catch m y h, acc)
+  | isValue m = Just (m, acc) -- `m` successfully evaluates to a value
+  | case m of
+      Throw w -> Just (subst y w h, acc) -- Handle exception by substitution
+      _       -> fmap (\(m', acc') -> (Catch m' y h, acc')) (smallStep (m, acc)) 
 smallStep _ = Nothing -- Catch-all case
 
 
