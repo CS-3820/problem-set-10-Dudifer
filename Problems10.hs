ad4fc5734ff27010019f01fb55038c7dc7be3608
janyberg <janyberg@uiowa.edu>
2024-11-15 23:35:08 -0600

    Update Problems10.hs

--- Problems10.hs
+++ Problems10.hs
@@ -212,7 +212,7 @@ smallStep (Plus m1 m2, acc)
   | isValue m1 = fmap (\(m2', acc') -> (Plus m1 m2', acc')) (smallStep (m2, acc))
   | otherwise  = fmap (\(m1', acc') -> (Plus m1' m2, acc')) (smallStep (m1, acc))
 smallStep (Store m, acc)
-  | isValue m = Just (Const 0, m) -- Replace accumulator with `m`'s value
+  | isValue m = Just (m, m) -- Replace accumulator with `m`'s value
   | otherwise = fmap (\(m', acc') -> (Store m', acc')) (smallStep (m, acc))
 smallStep (Recall, acc) = Just (acc, acc) -- Return current accumulator
 smallStep (Throw m, acc)
@@ -229,7 +229,7 @@ smallStep (Catch m y h, acc)
   | otherwise = case m of
       Throw w -> Just (subst y w h, acc) -- Handle exception by substitution
       _       -> fmap (\(m', acc') -> (Catch m' y h, acc')) (smallStep (m, acc))
-smallStep (e, acc) = Nothing -- Fallback case
+smallStep (e, acc) = Nothing
 
 
 steps :: (Expr, Expr) -> [(Expr, Expr)]
