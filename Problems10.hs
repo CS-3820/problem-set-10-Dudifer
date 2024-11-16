efa8659407c0b2371b00654e8c8d4a533b8d47ee
janyberg <janyberg@uiowa.edu>
2024-11-15 18:19:33 -0600

    Update Problems10.hs

--- Problems10.hs
+++ Problems10.hs
@@ -221,7 +221,7 @@ smallStep (Throw m, acc)
 smallStep (App (Lam x body) arg, acc)
   | isValue arg = Just (subst x arg body, acc) -- Function application
   | otherwise = fmap (\(arg', acc') -> ((App (Lam x body) arg)' acc')) (smallStep (arg, acc))
-smallStep (App m1 m2), acc
+smallStep (App m1 m2) acc
   | isValue m1 = fmap (\(m2', acc') -> (App m1 m2', acc')) (smallStep (m2, acc))
   | otherwise = fmap (\(m1', acc') -> (App m1' m2, acc')) (smallStep (m1, acc))
 smallStep (Catch m y h, acc)
